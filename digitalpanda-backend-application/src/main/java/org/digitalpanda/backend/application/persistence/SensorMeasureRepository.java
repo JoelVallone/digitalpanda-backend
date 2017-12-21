e0504eaf7f8780886e7d43c39e733c7d7e1cf8f4
Joël Vallone <joel.vallone@gmail.com>
2017-12-21 13:47:38 +0100

    Added sensor keys REST endpoint

--- digitalpanda-backend-application/src/main/java/org/digitalpanda/backend/application/persistence/SensorMeasureRepository.java
+++ digitalpanda-backend-application/src/main/java/org/digitalpanda/backend/application/persistence/SensorMeasureRepository.java
@@ -2,9 +2,12 @@ package org.digitalpanda.backend.application.persistence;
 
 import org.digitalpanda.backend.data.SensorMeasure;
 import org.digitalpanda.backend.data.SensorMeasureMetaData;
+import org.digitalpanda.backend.data.SensorMeasureType;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class SensorMeasureRepository {
@@ -18,12 +21,30 @@ public class SensorMeasureRepository {
 
         System.out.print("repository.get : ");
         SensorMeasure sensorMeasure = measuresMap.get(measureKey);
+        //SensorMeasure sensorMeasure = generateDummyMeasure(measureKey);
         if(sensorMeasure != null){
             System.out.println(measureKey + ", " + sensorMeasure);
         }else{
             System.out.println(measureKey + "=> no data");
         }
         return sensorMeasure;
+    }
+
+    public synchronized List<SensorMeasureMetaData> getKeys(){
+        System.out.println("repository.getKeys : ");
+        return new ArrayList<>(measuresMap.keySet());
+        //return getDummyMeasureKeys();
+    }
+
+    private List<SensorMeasureMetaData> getDummyMeasureKeys(){
+        List<SensorMeasureMetaData> keyList = new ArrayList<>();
+        keyList.add(new SensorMeasureMetaData("indoor", SensorMeasureType.TEMPERATURE));
+        keyList.add(new SensorMeasureMetaData("indoor", SensorMeasureType.PRESSURE));
+        keyList.add(new SensorMeasureMetaData("outdoor", SensorMeasureType.TEMPERATURE));
+        return keyList;
+    }
+    private SensorMeasure generateDummyMeasure(SensorMeasureMetaData measureKey) {
+        return new SensorMeasure(System.currentTimeMillis(), Math.random());
     }
 
     public synchronized void setMeasure(SensorMeasureMetaData measureKey, SensorMeasure sensorMeasure){
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTS_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JAR_OUTPUT_FOLDER="${SCRIPTS_FOLDER}/../../digitalpanda-infrastructure/docker/images/backend-java"
-
+cd ${SCRIPTS_FOLDER}/../
 mvn clean install
 cp "${SCRIPTS_FOLDER}/../digitalpanda-backend-application/target/"*backend*.jar "${JAR_OUTPUT_FOLDER}/"
